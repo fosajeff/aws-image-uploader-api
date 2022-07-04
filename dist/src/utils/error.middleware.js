3435147631a720713429a8a48322558243f46ce4
Efosa <okoobohefosa@gmail.com>
2022-07-04 18:26:24 +0100

    configure postinstall script

--- dist/src/utils/error.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorMiddleware = (error, req, res, next) => {
-    const status = error.status || 500;
-    const message = error.message || "Something went wrong";
-    res.status(status).send({ status: false, message });
-};
-exports.default = errorMiddleware;
