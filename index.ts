fd43a90c330ee1e27e1e5be00b874fb8a5ad1140
Efosa Jeffrey <okoobohefosa@gmail.com>
2022-07-06 13:57:34 +0100

    Dotenv only in dev

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== 'production') import "dotenv/config";
 import UploadController from "./src/resources/upload.controller";
 import App from "./app";
+if (process.env.NODE_ENV !== 'production') require("dotenv").config();
 
 const app = new App([new UploadController()], Number(process.env.PORT) || 4040);
 
